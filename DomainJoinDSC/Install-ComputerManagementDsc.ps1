0a8dc4cad253654f1080d148d71290e04715a33e
Rob Davies <rob.davies@outlook.com>
2019-02-13 09:04:46 -0800

    Update Install-ComputerManagementDsc.ps1

--- DomainJoinDSC/Install-ComputerManagementDsc.ps1
+++ DomainJoinDSC/Install-ComputerManagementDsc.ps1
@@ -4,6 +4,9 @@ param (
 )
  
 $password =  ConvertTo-SecureString $vmAdminPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential(".\$vmAdminUsername", $password)
- 
-Install-Module -Name ComputerManagementDsc -Force -AllowClobber -Confirm:$False -SkipPublisherCheck -Credential $credential
+$credential = New-Object System.Management.Automation.PSCredential(".\$($vmAdminUsername)", $password)
+
+Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $PSScriptRoot -ScriptBlock {
+  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+  Install-Module -Name ComputerManagementDsc -Force -AllowClobber -Confirm:$False -SkipPublisherCheck
+}
