c53cf08087cbf11f8381573c773312c247e2c42b
Rob Davies <rob.davies@outlook.com>
2019-02-13 08:25:23 -0800

    Update Install-ComputerManagementDsc.ps1

--- DomainJoinDSC/Install-ComputerManagementDsc.ps1
+++ DomainJoinDSC/Install-ComputerManagementDsc.ps1
@@ -1,1 +1,9 @@
-Install-Module -Name ComputerManagementDsc -Force -AllowClobber -Confirm:$False -SkipPublisherCheck
+param (
+  $vmAdminUsername,
+  $vmAdminPassword
+)
+ 
+$password =  ConvertTo-SecureString $vmAdminPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential(".\$vmAdminUsername", $password)
+ 
+Install-Module -Name ComputerManagementDsc -Force -AllowClobber -Confirm:$False -SkipPublisherCheck -Credential $credential
